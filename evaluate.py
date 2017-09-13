ed27c3f48c09ac8e1ec6f878f1105fdd81e5f87e
Ligin Vellakkad <ligindd@gmail.com>
2017-09-13 13:46:12 +0530

    Add postfix evaluation function.

---
+++ evaluate.py
@@ -0,0 +1,32 @@
+from sys import argv
+
+postfix = argv[1]
+
+print(f"The espression is : {postfix} {type(postfix)}")
+
+
+def evaluate(expression):
+    stack = []
+    for i in expression:
+        # print(i)
+        if i.isdigit():
+            stack.append(i)
+        else:
+            operand_One = int(stack.pop())
+            operator = i
+            operand_Two = int(stack.pop())
+
+            operators = {'+': operand_One + operand_Two,
+                         '-': operand_One - operand_Two,
+                         '*': operand_One * operand_Two,
+                         '/': operand_One / operand_Two
+                         }
+
+            if operators[i]:
+                stack.append(operators[i])
+            else:
+                print("Wrong expression!")
+
+    print(f"{stack}")
+
+evaluate(postfix)
